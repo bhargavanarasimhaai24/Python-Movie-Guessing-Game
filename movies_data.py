6905ed7234f6478473d08da1b5fd6e86ab1b95cc
Kopparapu Bhargava Narasimha <bhargavanarasimha.ai24@bmsce.ac.in>
2026-02-14 21:13:48 +0530

    Create movies_data.py

---
+++ movies_data.py
@@ -0,0 +1,134 @@
+english = {
+
+    "Inception": {
+        "hero": "Leonardo DiCaprio",
+        "heroine": "Elliot Page",
+        "dialogue": "You mustn't be afraid to dream a little bigger, darling.",
+        "song": "Time - Hans Zimmer",
+        "poster": "posters/inception.jpg"
+    },
+
+    "Titanic": {
+        "hero": "Leonardo DiCaprio",
+        "heroine": "Kate Winslet",
+        "dialogue": "I'm the king of the world!",
+        "song": "My Heart Will Go On",
+        "poster": "posters/titanic.jpg"
+    },
+
+    "The Dark Knight": {
+        "hero": "Christian Bale",
+        "heroine": "Maggie Gyllenhaal",
+        "dialogue": "Why so serious?",
+        "song": "Why So Serious - Hans Zimmer",
+        "poster": "posters/dark_knight.jpg"
+    },
+
+    "Avengers Endgame": {
+        "hero": "Robert Downey Jr.",
+        "heroine": "Scarlett Johansson",
+        "dialogue": "I am Iron Man.",
+        "song": "Portals - Alan Silvestri",
+        "poster": "posters/endgame.jpg"
+    },
+
+    "Spider Man No Way Home": {
+        "hero": "Tom Holland",
+        "heroine": "Zendaya",
+        "dialogue": "With great power comes great responsibility.",
+        "song": "Spider-Man Theme",
+        "poster": "posters/spiderman.jpg"
+    },
+
+    "Harry Potter and the Sorcerers Stone": {
+        "hero": "Daniel Radcliffe",
+        "heroine": "Emma Watson",
+        "dialogue": "You're a wizard, Harry.",
+        "song": "Hedwig's Theme",
+        "poster": "posters/harry_potter.jpg"
+    },
+
+    "La La Land": {
+        "hero": "Ryan Gosling",
+        "heroine": "Emma Stone",
+        "dialogue": "Here's to the fools who dream.",
+        "song": "City of Stars",
+        "poster": "posters/lalaland.jpg"
+    },
+
+    "Frozen": {
+        "hero": "Jonathan Groff",
+        "heroine": "Idina Menzel",
+        "dialogue": "Let it go!",
+        "song": "Let It Go",
+        "poster": "posters/frozen.jpg"
+    },
+
+    "Joker": {
+        "hero": "Joaquin Phoenix",
+        "heroine": "Zazie Beetz",
+        "dialogue": "You get what you deserve!",
+        "song": "Rock and Roll Part 2",
+        "poster": "posters/joker.jpg"
+    }
+}
+
+
+hindi = {
+
+    "Dangal": {
+        "hero": "Aamir Khan",
+        "heroine": "Fatima Sana Shaikh",
+        "dialogue": "Gold to gold hota hai.",
+        "song": "Dhaakad",
+        "poster": "posters/dangal.jpg"
+    },
+
+    "3 Idiots": {
+        "hero": "Aamir Khan",
+        "heroine": "Kareena Kapoor",
+        "dialogue": "All is well.",
+        "song": "Give Me Some Sunshine",
+        "poster": "posters/3idiots.jpg"
+    }
+}
+
+
+telugu = {
+
+    "RRR": {
+        "hero": "NTR Jr",
+        "heroine": "Alia Bhatt",
+        "dialogue": "Etthara Jenda!",
+        "song": "Naatu Naatu",
+        "poster": "posters/rrr.jpg"
+    },
+
+    "Baahubali The Beginning": {
+        "hero": "Prabhas",
+        "heroine": "Tamannaah",
+        "dialogue": "Shivudu!",
+        "song": "Dheevara",
+        "poster": "posters/baahubali1.jpg"
+    }
+}
+
+
+kannada = {
+
+    "KGF Chapter 2": {
+        "hero": "Yash",
+        "heroine": "Srinidhi Shetty",
+        "dialogue": "The world is my territory.",
+        "song": "Toofan",
+        "poster": "posters/kgf2.jpg"
+    },
+
+    "Kantara": {
+        "hero": "Rishab Shetty",
+        "heroine": "Sapthami Gowda",
+        "dialogue": "Daiva is powerful.",
+        "song": "Varaha Roopam",
+        "poster": "posters/kantara.jpg"
+    }
+}
